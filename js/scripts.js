d1176ba3d99f4227c419d38319c4392ee0b00063
akkharachai wareerat <akkmik095@gmail.com>
2024-02-07 18:19:18 +0700

    oooo

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,42 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+// function slide() {
+//     // เลือก element ที่มี class "slider"
+//     var slider = document.querySelector('.slider');
+//     // เลื่อนรูปภาพที่อยู่ใน "slider" ไปทางขวา
+//     slider.scrollLeft += slider.offsetWidth;
+//     // ถ้าเลื่อนไปถึงสุดท้าย ให้เลื่อนกลับไปที่ตำแหน่งแรก
+//     if (slider.scrollLeft >= (slider.scrollWidth - slider.offsetWidth)) {
+//       slider.scrollLeft = 0;
+//     }
+//     setInterval(slide, 3000);
+//   }
+
+  // เรียกใช้ฟังก์ชัน slide() ทุก 3 วินาที (3000 มิลลิวินาที)
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+
+let index = 0;
+
+function slide() {
+    index = (index + 1) % images.length;
+    const offset = -index * 100;
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(slide, 3500); // เปลี่ยนรูปทุก 3 วินาที
+
+
+const slider1 = document.querySelector('.slider1');
+const images1 = document.querySelectorAll('.slider1 img');
+
+let index1 = 0;
+
+function slide1() {
+    index1 = (index1 + 1) % images1.length;
+    const offset = -index1 * 100;
+    slider1.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(slide1, 3500); // เปลี่ยนรูปทุก 3 วินาที
